3b6d5cda1fc4791098f37928c5e33ab8cea2a210
Miheel <mikaelleuf@gmail.com>
2023-03-01 15:43:33 +0100

    added LunchResource and named query in lunch entity for whole weeks lunch

---
+++ src/main/java/RestAPI/Resources/LunchResource.java
@@ -0,0 +1,27 @@
+package RestAPI.Resources;
+
+import Entity.TodaysLunch;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@ApplicationScoped
+@Transactional(Transactional.TxType.REQUIRED)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/lunch")
+public class LunchResource {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @GET
+    @Path("/week")
+    public Response getWeeksLunch() {
+        return Response.ok(em.createNamedQuery("Lunch.week", TodaysLunch.class).getResultList()).build();
+    }
+}
